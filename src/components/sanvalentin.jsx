f4b7c599b1ac8807310e5041cde59d92736c5143
Javier Perren <javi_x90@yahoo.com>
2025-02-13 13:14:01 -0300

    valentinFinal

--- src/components/sanvalentin.jsx
+++ src/components/sanvalentin.jsx
@@ -6,7 +6,7 @@ export default function ValentineOnePage() {
   const audioRef = useRef(null);
   const message = `Brenda, mi amor..\n\nDesde aquel día que nos vimos en el Cosmo, supe que había algo especial en ti. No imaginé que ese encuentro nos llevaría a este hermoso camino juntos, pero ahora no puedo imaginar mi vida sin ti.\n\nCada día a tu lado es un regalo, y desde el 25 de diciembre, mi mundo brilla más fuerte. Adoro tu risa, tu mirada y la forma en que haces que todo sea más bonito y significativo.\n\nEste es nuestro primer San Valentín juntos, y quiero que sepas que es el primero de muchos. Gracias por ser mi compañera, por cada momento compartido y por todo lo que nos espera.\n\nTe quiero con todo mi corazón.\n\nFeliz San Valentín, mi amor.\n\nJavier 💖`;
 
-  const speed = 10;
+  const speed = 60;
   const step = 1;
 
   useEffect(() => {
