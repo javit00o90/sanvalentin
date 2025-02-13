a19d1f8363b0a7dbc8c6621e5125bdbeb6e594e4
Javier Perren <javi_x90@yahoo.com>
2025-02-13 12:51:24 -0300

    sanValen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+// import ValentineOnePage from "./components/sanvalentin";
+
+// function App() {
+//   return <ValentineOnePage />;
+// }
+
+// export default App;
+
+import { useState } from "react";
 import ValentineOnePage from "./components/sanvalentin";
 
 function App() {
-  return <ValentineOnePage />;
+  const [showLetter, setShowLetter] = useState(false);
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-pink-100">
+      {!showLetter ? (
+        <button onClick={() => setShowLetter(true)} className="relative botonCarta">
+          <img src="/carta1.png" alt="Sobre de carta" className="w-40 h-40 cursor-pointer transition-transform duration-500 hover:scale-105 imagenCarta" />
+        </button>
+      ) : (
+        <ValentineOnePage />
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/components/sanvalentin.jsx
+++ src/components/sanvalentin.jsx
@@ -3,7 +3,6 @@ import "leaflet/dist/leaflet.css";
 
 export default function ValentineOnePage() {
   const [text, setText] = useState("");
-  const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef(null);
   const message = `Brenda, mi amor..\n\nDesde aquel día que nos vimos en el Cosmo, supe que había algo especial en ti. No imaginé que ese encuentro nos llevaría a este hermoso camino juntos, pero ahora no puedo imaginar mi vida sin ti.\n\nCada día a tu lado es un regalo, y desde el 25 de diciembre, mi mundo brilla más fuerte. Adoro tu risa, tu mirada y la forma en que haces que todo sea más bonito y significativo.\n\nEste es nuestro primer San Valentín juntos, y quiero que sepas que es el primero de muchos. Gracias por ser mi compañera, por cada momento compartido y por todo lo que nos espera.\n\nTe quiero con todo mi corazón.\n\nFeliz San Valentín, mi amor.\n\nJavier 💖`;
 
@@ -26,21 +25,10 @@ export default function ValentineOnePage() {
     return () => clearInterval(interval);
   }, [message]);
 
-  const handlePlayAudio = () => {
-    if (audioRef.current) {
-      audioRef.current.volume = 0.3;
-      audioRef.current.play();
-      setIsPlaying(true);
-    }
-  };
 
   return (
     <div className="flex flex-col items-center justify-center w-full max-w-lg">
-      {!isPlaying && (
-        <button className="botonMusica" onClick={handlePlayAudio}>
-        </button>
-      )}
-
+      
       {/* Componente de audio */}
       <audio ref={audioRef} autoPlay loop>
         <source src="/cancion.mp3" type="audio/mp3" />
